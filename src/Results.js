04a9200c32f6927cfc7e4ff299479a8e9b92bfe1
Md Abu Zafar <mdabuzafar78@gmail.com>
2021-03-13 02:01:46 +0530

    Learn about tailwindcss

--- src/Results.js
+++ src/Results.js
@@ -3,11 +3,7 @@ import Pet from "./Pet";
 
 export default function Results({ pets }) {
   return (
-    <div className="search">
-      {/* {pets.map(({ id: key, name, animal, breed }) => (
-        <Pet name={name} animal={animal} breed={breed} key={key} />
-      ))} */}
-
+    <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
       {!pets.length ? (
         <h2>No Pets Found</h2>
       ) : (
